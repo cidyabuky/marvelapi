de6908e43a821a6461ba8c82a0eae0cbadc9d6d9
Alcides Mendes <alcidescm@gmail.com>
2016-11-22 12:55:24 -0200

    Ajuste do getIdThumbnail

--- src/main/java/br/com/marvelapi/model/Thumbnail.java
+++ src/main/java/br/com/marvelapi/model/Thumbnail.java
@@ -4,66 +4,82 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity(name = "thumbnail")
 public class Thumbnail {
 
-	private static final String PORTRAIT_XLARGE = "/portrait_xlarge.";
-	private static final String PORTRAIT_MEDIUM = "/portrait_medium.";
+    private static final String PORTRAIT_XLARGE = "/portrait_xlarge.";
+    private static final String PORTRAIT_MEDIUM = "/portrait_medium.";
 
-	@JsonIgnore
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer idThumbnail;
+    @JsonIgnore
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idThumbnail;
 
-	private String path;
+    private String path;
 
-	private String extension;
-	
-	@OneToOne(mappedBy = "thumbnail")
-	private Characters characters;
+    private String extension;
 
-	public Integer getIdThumbnail() {
-		return idThumbnail;
-	}
+    @OneToOne(mappedBy = "thumbnail")
+    private Characters characters;
 
-	public void setIdThumbnail(Integer idThumbnail) {
-		this.id = idThumbnail;
-	}
+    public Integer getIdThumbnail() {
+        return idThumbnail;
+    }
 
-	public String getPath() {
-		return path;
-	}
+    public void setIdThumbnail(Integer idThumbnail) {
+        this.idThumbnail = idThumbnail;
+    }
 
-	public void setPath(String path) {
-		this.path = path;
-	}
+    public String getPath() {
+        return path;
+    }
 
-	public String getExtension() {
-		return extension;
-	}
+    public void setPath(String path) {
+        this.path = path;
+    }
 
-	public void setExtension(String extension) {
-		this.extension = extension;
-	}
+    public String getExtension() {
+        return extension;
+    }
 
-	private String getUrlImage(String size) {
-		return getPath() + size + getExtension();
-	}
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
 
-	public String getUrlImageLarge() {
-		return getUrlImage(PORTRAIT_XLARGE);
-	}
+    /**
+     * @return Retorna characters
+     */
+    public Characters getCharacters() {
+        return characters;
+    }
 
-	public String getUrlImageMedium() {
-		return getUrlImage(PORTRAIT_MEDIUM);
-	}
+    /**
+     * @param characters
+     *            Define characters
+     */
+    public void setCharacters(Characters characters) {
+        this.characters = characters;
+    }
 
-	@Override
-	public String toString() {
-		return "Thumbnail [path=" + path + ", extension=" + extension + "]";
-	}
+    private String getUrlImage(String size) {
+        return getPath() + size + getExtension();
+    }
+
+    public String getUrlImageLarge() {
+        return getUrlImage(PORTRAIT_XLARGE);
+    }
+
+    public String getUrlImageMedium() {
+        return getUrlImage(PORTRAIT_MEDIUM);
+    }
+
+    @Override
+    public String toString() {
+        return "Thumbnail [path=" + path + ", extension=" + extension + "]";
+    }
 
 }
