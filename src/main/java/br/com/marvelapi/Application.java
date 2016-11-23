1f59e48432b09c29aa2f7998597f76a9a9ff88cc
Alcides Cruz Mendes <alcidescm@gmail.com>
2016-11-23 02:18:18 -0200

    Ajustes para publicação

--- src/main/java/br/com/marvelapi/Application.java
+++ src/main/java/br/com/marvelapi/Application.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 @SpringBootApplication
-public class Application {
-	
+public class Application extends SpringBootServletInitializer {
+
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) throws Throwable {
@@ -20,5 +22,11 @@ public class Application {
 	@Bean
 	public SpringDataDialect springDataDialect() {
 		return new SpringDataDialect();
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(Application.class);
 	}
 }
